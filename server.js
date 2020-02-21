01825a58dc8136f9b27869ed7031de00e3498a57
PHfalcom <phfalcom@gmail.com>
2020-02-21 13:14:15 -0300

    Sistema de Cadastro

--- server.js
+++ server.js
@@ -1,17 +1,70 @@
 const express = require('express')
 const server = express()
-const nunjucks = require('nunjucks')
-
-
-
-nunjucks.configure('./', {
-    express: server
-})
 
 server.use(express.static('public'))
-server.get("/", function (req, res) {
-    return res.render('index.html')
+server.use(express.urlencoded({ extended: true }))
+
+
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '1724',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
+const nunjucks = require('nunjucks')
+nunjucks.configure('./', {
+    express: server,
+    noCache: true,
 })
 
 
-server.listen(3000)
+server.get("/", function (req, res) {
+
+    db.query("SELECT * FROM donors", function (err, result) {
+        if (err) return res.send("Erro de banco de Dados.")
+
+        const donors = result.rows
+        return res.render("index.html", { donors })
+    })
+
+
+})
+
+server.post('/', function (req, res) {
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    if (name == '' || email == '' || blood == '') {
+        return res.send('Todos os campos são obrigatórios.')
+    }
+
+    const query = `
+    INSERT INTO donors ("name","email","blood") 
+    VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function (err) {
+        if (err) return res.send('erro no banco de Dados.')
+
+        return res.redirect('/')
+
+    })
+
+
+
+})
+
+server.listen(3000, function () {
+    console.log('servidor on')
+})
+
+
+
+
+
+
